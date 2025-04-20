8fbe0b53faf7de6222ef5e1d704d460d61f2d4db
Vi Do <havido0611@gmail.com>
2025-04-20 13:27:18 -0700

    done

--- src/index.js
+++ src/index.js
@@ -13,6 +13,23 @@ const fossilfuel = document.querySelector('.fossil-fuel');
 const myregion = document.querySelector('.my-region');
 const clearBtn = document.querySelector('.clear-btn');
 
+function calculateColor(value) {
+	let co2Scale = [0, 150, 600, 750, 800];
+	let colors = ['#2AA364', '#F5EB4D', '#9E4229', '#381D02', '#381D02'];
+
+	let closestNum = co2Scale.sort((a, b) => {
+		return Math.abs(a - value) - Math.abs(b - value);
+	})[0];
+	console.log(value + ' is closest to ' + closestNum);
+	let num = (element) => element > closestNum;
+	let scaleIndex = co2Scale.findIndex(num);
+
+	let closestColor = colors[scaleIndex];
+	console.log(scaleIndex, closestColor);
+
+	chrome.runtime.sendMessage({ action: 'updateIcon', value: { color: closestColor } });
+}
+
 //6
 //call the API
 import axios from '../node_modules/axios';
@@ -31,7 +48,7 @@ async function displayCarbonUsage(apiKey, region) {
             .then((response) => {
                 console.log(response);
                 let CO2 = Math.floor(response.data.carbonIntensity);
-                // calculateColor(CO2);
+                calculateColor(CO2);
                 loading.style.display = 'none';
                 form.style.display = 'none';
                 myregion.textContent = region;
@@ -69,6 +86,13 @@ function handleSubmit(e) {
 
 //3 initial checks
 function init() {
+    chrome.runtime.sendMessage({
+        action: 'updateIcon',
+        value: {
+            color: 'green',
+        },
+    });
+    
 	//if anything is in localStorage, pick it up
 	const storedApiKey = localStorage.getItem('apiKey');
 	const storedRegion = localStorage.getItem('regionName');
